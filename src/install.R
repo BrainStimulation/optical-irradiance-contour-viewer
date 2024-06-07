a4c3dcb10af3418bf110194106a32d7dfe042c2c
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-06-07 23:12:08 +1200

    Install shinylive paackage.

--- src/install.R
+++ src/install.R
@@ -1,5 +1,6 @@
 cat("Installing packages, this may take a few minutes...\n\n\n\n")
 
+install.packages("shinylive", repos = "http://cran.us.r-project.org")
 install.packages("shiny", repos = "http://cran.us.r-project.org")
 install.packages("bslib", repos = "http://cran.us.r-project.org")
 install.packages("R.matlab", repos = "http://cran.us.r-project.org")
